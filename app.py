1dd27f71c853d6eefb5ff3feb53125b9b2406b1c
Jhon Ross <jhonross.lp@outlook.com>
2025-05-30 23:52:16 -0300

    fix

--- app.py
+++ app.py
@@ -399,4 +399,4 @@ def success():
 if __name__ == '__main__':
     # ... (suas verificações e logs)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
